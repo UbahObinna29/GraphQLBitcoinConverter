d76b820f9a34db80a576692d1f6af5fc5afcce27
Obinna Ubah <cubah@teamapt.com>
2019-10-22 13:12:24 +0100

    1. Renamed project to BitcoinConverter.

--- src/main/java/com/nebula/currencyconverter/BitcoinConverterApplication.java
+++ src/main/java/com/nebula/currencyconverter/BitcoinConverterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyConverterApplication {
+public class BitcoinConverterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyConverterApplication.class, args);
+		SpringApplication.run(BitcoinConverterApplication.class, args);
 	}
 
 }
